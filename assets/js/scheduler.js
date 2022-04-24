a8f1f08de8b511c5b0be0a78f58020eb46c7b1a1
Jeongho Lee <jeongholee@Jeonghos-iMac-2.local>
2022-04-23 21:53:50 -0700

    fixed storge problems

--- assets/js/scheduler.js
+++ assets/js/scheduler.js
@@ -29,16 +29,16 @@ $(".saveBtn").on("click", function() {
 });
 
 // Get Item from Local Storage
-$("#8am .plan").val(localStorage.getItem("#8am"));
-$("#9am .plan9am").val(localStorage.getItem("#9am"));
-$("#10am .plan").val(localStorage.getItem("#10am"));
-$("#11am .plan").val(localStorage.getItem("#11am"));
-$("#12pm .plan").val(localStorage.getItem("#12pm"));
-$("#1pm .plan").val(localStorage.getItem("#1pm"));
-$("#2pm .plan").val(localStorage.getItem("#2pm"));
-$("#3pm .plan").val(localStorage.getItem("#3pm"));
-$("#4pm .plan").val(localStorage.getItem("#4pm"));
-$("#5pm .plan").val(localStorage.getItem("#5pm"));
-$("#6pm .plan").val(localStorage.getItem("#6pm"));
-$("#7pm .plan").val(localStorage.getItem("#7pm"));
-$("#8pm .plan").val(localStorage.getItem("#8pm"));
+$("#8am .plan").val(localStorage.getItem("8am"));
+$("#9am .plan").val(localStorage.getItem("9am"));
+$("#10am .plan").val(localStorage.getItem("10am"));
+$("#11am .plan").val(localStorage.getItem("11am"));
+$("#12pm .plan").val(localStorage.getItem("12pm"));
+$("#1pm .plan").val(localStorage.getItem("1pm"));
+$("#2pm .plan").val(localStorage.getItem("2pm"));
+$("#3pm .plan").val(localStorage.getItem("3pm"));
+$("#4pm .plan").val(localStorage.getItem("4pm"));
+$("#5pm .plan").val(localStorage.getItem("5pm"));
+$("#6pm .plan").val(localStorage.getItem("6pm"));
+$("#7pm .plan").val(localStorage.getItem("7pm"));
+$("#8pm .plan").val(localStorage.getItem("8pm"));
